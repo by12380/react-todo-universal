2c1dbd3e7b8cc1918b4ff08ba5113b847c77722c
Brian Young <by12380@gmail.com>
2018-08-30 20:55:58 -0700

    Create todo page and components

---
+++ Client/React/src/components/TodoItem/component.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import './todoItem.css';
+
+class TodoItem extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isEdit: false,
+        }
+    }
+
+    handleEditTitle = () => {
+        this.setState({
+            isEdit: true
+        })
+    }
+
+    handleSubmitTitle = () => {
+        this.props.updateItem(
+            this.props.token, 
+            {
+                _id: this.props.id,
+                title: this.input.value
+            }
+        )
+
+        this.setState({
+            isEdit: false
+        })
+    }
+
+    handleCheck = () => {
+        this.props.updateItem(
+            this.props.token, 
+            {
+                _id: this.props.id,
+                completed: !this.props.completed
+            }
+        )
+
+        this.setState({
+            isEdit: false
+        })
+    }
+
+    handleDelete = () => {
+        this.props.deleteItem(this.props.token, {_id: this.props.id})
+    }
+
+    renderTitle = () => {
+        return this.state.isEdit
+            ?   <input
+                    ref={input => this.input = input}
+                    className="edit-title"
+                    type="text"
+                    defaultValue={this.props.title}
+                    onBlur={this.handleSubmitTitle}
+                    autoFocus
+                />
+            :   <p 
+                    className={this.props.completed ? "title-crossed" : "title"}
+                    onDoubleClick={this.props.completed ? null : this.handleEditTitle}>
+                        {this.props.title}
+                </p>
+    }
+
+    renderCheckIcon = () => {
+        return this.props.completed
+            ? <span>v</span>
+            : null
+    }
+
+    render() {
+        return (
+            <div className="item-container">
+                <div className="completed-check-box" onClick={this.handleCheck}>
+                    {this.renderCheckIcon()}
+                </div>
+                <div className="title-container">
+                    {this.renderTitle()}
+                </div>
+                <div className="delete-btn" onClick={this.handleDelete}>x</div>
+            </div>
+        )
+    }
+}
+
+export default TodoItem;
