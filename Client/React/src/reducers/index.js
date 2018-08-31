e1e619d92b47fa45c59478268964f7f2ac5ab7ab
Brian Young <by12380@gmail.com>
2018-08-30 20:50:22 -0700

    Create todo CRUD actions/reducers

--- Client/React/src/reducers/index.js
+++ Client/React/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from './authReducer';
 import userReducer from './userReducer';
+import todoReducer from './todoReducer';
 
 export default combineReducers({
     authReducer,
-    userReducer
+    userReducer,
+    todoReducer
 });
