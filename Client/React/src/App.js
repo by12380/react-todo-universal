3ee5421894af5259fb6754c71f8d93757a4de4c5
Brian Young <by12380@gmail.com>
2018-08-30 20:59:19 -0700

    Add navigation to todo page

--- Client/React/src/App.js
+++ Client/React/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect, NavLink} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import Profile from './components/Profile';
 import LogIn from './components/LogIn';
 import Callback from './components/Callback';
+import Todo from './components/Todo';
 import { loadSession } from './actions/authActions';
 
 import logo from './logo.svg';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 class App extends Component {
 
@@ -23,12 +25,16 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </div>
+          <nav className="navbar">
+            <ul>
+              <li><NavLink exact to="/">Todo</NavLink></li>
+              <li><NavLink to="/profile">Profile</NavLink></li>
+            </ul>
+          </nav>
           <main>
             <div>
-              <Route exact path="/" render={() => this.props.isAuthenticated ? <Profile /> : <Redirect to='/login' />} />
+              <Route exact path="/" render={() => this.props.isAuthenticated ? <Todo /> : <Redirect to='/login' />} />
+              <Route exact path="/profile" render={() => this.props.isAuthenticated ? <Profile /> : <Redirect to='/login' />} />
               <Route exact path="/login" render={() => !this.props.isAuthenticated ? <LogIn /> : <Redirect to='/' />} />
               <Route exact path="/callback" component={Callback} />
             </div>
