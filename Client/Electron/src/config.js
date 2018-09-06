e2eab500c97304775da6b309cf1d5a62954d2ab5
Brian Young <by12380@gmail.com>
2018-09-06 15:14:43 -0700

    Update config

--- Client/Electron/src/config.js
+++ Client/Electron/src/config.js
@@ -1,7 +1,7 @@
-export const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN || 'react-universal.auth0.com';
-export const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID || '3821inroTsXub5iKOew7JHTT-OaJl3jp';
-export const AUTH0_CLIENT_SECRET = process.env.REACT_APP_AUTH0_CLIENT_SECRET || '3usxeq-Ae3jur3LYVlhJ0Ldjyi01sCoHsbTdRpbFLFwzeRMZjQHCp8La1ixDx9jW';
-export const AUTH0_API_AUDIENCE = process.env.REACT_APP_AUTH0_API_AUDIENCE || 'https://api.react-universal.com';
+export const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN || 'react-todo-universal.auth0.com';
+export const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID || 'tBLDtlPmAIQTKk9MO1gIbZ5ibLfs85ZL';
+export const AUTH0_CLIENT_SECRET = process.env.REACT_APP_AUTH0_CLIENT_SECRET || 'c5OEcxsL6Vj9KuuWTrvGfvqZmg3NW1ejQFsP7VjvJSgvew2IGv8OOGO0ydCGmknz';
+export const AUTH0_API_AUDIENCE = process.env.REACT_APP_AUTH0_API_AUDIENCE || 'https://api.react-todo-universal.com';
 export const AUTH0_LOGIN_REDIRECT_URL = process.env.REACT_APP_AUTH0_LOGIN_REDIRECT_URL || 'http://localhost:3000/callback';
 export const AUTH0_LOGOUT_REDIRECT_URL = process.env.REACT_APP_AUTH0_LOGOUT_REDIRECT_URL || 'http://localhost:3000/';
 
