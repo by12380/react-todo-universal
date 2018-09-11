6130e7f53851efc6671eeb79b2bc0d491b5dda9c
Brian Young <by12380@gmail.com>
2018-09-11 13:30:24 -0700

    Update button styles

--- Client/React/src/components/LogIn/component.js
+++ Client/React/src/components/LogIn/component.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { logIn, refreshAccessToken } from '../../utils/auth0';
+import { REACT_UNIVERSAL_REPO_URL } from '../../config';
 import './LogIn.css';
+
 
 class LogIn extends Component {
 
@@ -19,6 +21,10 @@ class LogIn extends Component {
     render() {
         return (
             <div>
+                <h1 class="site-header">Todos</h1>
+                <p className="app-description">
+                    Universal, cross platform todos app built from <a href={REACT_UNIVERSAL_REPO_URL} target="_blank">React Universal</a> starter kit
+                </p>
                 <button className='login-btn' onClick={this.onLogin}>
                     Log In
                 </button>
