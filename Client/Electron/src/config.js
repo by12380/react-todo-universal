e52d90a279883313b9674d834199f3504ef28f51
Brian Young <by12380@gmail.com>
2018-09-11 15:45:25 -0700

    Reset electron config

--- Client/Electron/src/config.js
+++ Client/Electron/src/config.js
@@ -1,9 +1,12 @@
-export const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN || 'react-todo-universal.auth0.com';
-export const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID || 'tBLDtlPmAIQTKk9MO1gIbZ5ibLfs85ZL';
-export const AUTH0_CLIENT_SECRET = process.env.REACT_APP_AUTH0_CLIENT_SECRET || 'c5OEcxsL6Vj9KuuWTrvGfvqZmg3NW1ejQFsP7VjvJSgvew2IGv8OOGO0ydCGmknz';
-export const AUTH0_API_AUDIENCE = process.env.REACT_APP_AUTH0_API_AUDIENCE || 'https://api.react-todo-universal.com';
+export const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN;
+export const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID;
+export const AUTH0_CLIENT_SECRET = process.env.REACT_APP_AUTH0_CLIENT_SECRET;
+export const AUTH0_API_AUDIENCE = process.env.REACT_APP_AUTH0_API_AUDIENCE;
 export const AUTH0_LOGIN_REDIRECT_URL = process.env.REACT_APP_AUTH0_LOGIN_REDIRECT_URL || 'http://localhost:3000/callback';
 export const AUTH0_LOGOUT_REDIRECT_URL = process.env.REACT_APP_AUTH0_LOGOUT_REDIRECT_URL || 'http://localhost:3000/';
 
 //App server
-export const APP_SERVER_URL = process.env.REACT_APP_APP_SERVER_URL || 'https://react-todo-server.herokuapp.com' || 'http://localhost:8080';
+export const APP_SERVER_URL = process.env.REACT_APP_APP_SERVER_URL || 'http://localhost:8080';
+
+//Asset links
+export const REACT_UNIVERSAL_REPO_URL = 'https://github.com/by12380/react-universal';
